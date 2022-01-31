ab35290c0cc5a184f9c6c3f68ea90a99fe4ede74
Christian Holm Christensen <chr100@gmail.com>
2022-01-31 22:51:46 +0100

    add button for clearing sketch

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".clearButton");
+
+button.addEventListener("click", (e) => {
+  //do something
+});
 
 function makeRows(rows, cols) {
-  for (c = 0; c < rows * cols; c++) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
   }
@@ -9,4 +16,10 @@ function makeRows(rows, cols) {
 
 makeRows(16, 16);
 
+const gridItem = document.querySelectorAll(".grid-item");
 
+document.querySelectorAll(".grid-item").forEach((item) => {
+  item.addEventListener("mouseenter", (e) => {
+    item.className = "black";
+  });
+});
